2c101b11457fbae39f8b0b15218f8600c3f0b68b
Kyle Cumming <kyle.cumming@dal.ca>
2022-03-07 15:37:06 -0500

    final lab6

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>My First React App</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Lab 6, CSCI3172, Kyle Cumming, B00773076
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
